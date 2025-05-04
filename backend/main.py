99d2572431244bda08b50e267c52054351f88376
Azhar <azhargamer15@gmail.com>
2025-05-04 20:43:50 +0200

    making a main.py
    this will run the entire backend of the project
    this will be able to store templates found and find them
    able to have links and site images for those templateds

--- backend/main.py
+++ backend/main.py
@@ -1,7 +1,16 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
 
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:3000"],  # Frontend address
+    allow_credentials=True,
+    allow_methods=["*"],  
+    allow_headers=["*"], 
+)
+
 @app.get("/")
 def read_root():
-    return {"message": "Welcome to the Template Fetcher API!"}
+    return {"message": "Hello, World!"}
