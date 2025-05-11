218b0c0c15ba53c730cdc5ed73d13a9f346b79d7
Azhar <azhargamer15@gmail.com>
2025-05-11 17:53:54 +0200

    update main.py

--- backend/main.py
+++ backend/main.py
@@ -1,17 +1,6 @@
 from fastapi import FastAPI
-from fastapi.middleware.cors import CORSMiddleware
 from routers import templates
 
 app = FastAPI()
 
-# Allow frontend (localhost:3000) to access backend
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins=["http://localhost:3000"],
-    allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"],
-)
-
-# Register router
-app.include_router(templates.router)
+app.include_router(templates.router, prefix="/api")
