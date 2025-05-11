b4c896c1e21c900cd2cfbdf02ea7c384c87b0fa7
Azhar <azhargamer15@gmail.com>
2025-05-11 17:54:14 +0200

    update template.py

--- backend/models/template.py
+++ backend/models/template.py
@@ -1,9 +1,7 @@
-from pydantic import BaseModel, HttpUrl
-from typing import Optional
+from pydantic import BaseModel
 
 class Template(BaseModel):
-    id: int
     title: str
     description: str
-    thumbnail: HttpUrl
-    url: HttpUrl
+    thumbnail: str
+    link: str
