7f33595f4452ae43721ec4b05406a7c146fb9b3d
Azhar <azhargamer15@gmail.com>
2025-05-26 17:20:58 +0200

    adding main.py built to run entire backend

--- backend/main.py
+++ backend/main.py
@@ -1,16 +1,20 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Query
 from fastapi.middleware.cors import CORSMiddleware
+from database import init_db, get_templates
 
 app = FastAPI()
+init_db()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000"],  
-    allow_credentials=True,
+    allow_origins=["*"],
     allow_methods=["*"],
     allow_headers=["*"],
 )
 
 @app.get("/templates")
-async def get_templates():
-    return {"results": ["Template 1", "Template 2"]}
+def search_templates(query: str = Query(..., min_length=1)):
+    templates = get_templates(query)
+    if not templates:
+        raise HTTPException(status_code=404, detail="No templates found.")
+    return {"results": templates}
